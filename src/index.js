b69a7d8024a6b0e35858475914c41bbc782591fa
Julian <vilaj46@gmail.com>
2020-09-21 07:43:24 -0400

    Added gh-pages and setup for deployment.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App/App";
 
+// Simple styling we will be going for:
+// https://www.w3schools.com/howto/howto_js_todolist.asp
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
