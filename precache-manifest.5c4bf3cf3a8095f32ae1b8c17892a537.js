8dd5e9683104b469a5b60ef21715fefc7a7b579c
Julian <vilaj46@gmail.com>
2020-09-21 07:41:42 -0400

    Updates

---
+++ precache-manifest.5c4bf3cf3a8095f32ae1b8c17892a537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678220bd8837b53fba25135b8f0d2b80",
+    "url": "/glorified-todo/index.html"
+  },
+  {
+    "revision": "bff786c93463a5d10922",
+    "url": "/glorified-todo/static/css/main.52c29046.chunk.css"
+  },
+  {
+    "revision": "b729c3c9b5ba085fe50c",
+    "url": "/glorified-todo/static/js/2.071232ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/glorified-todo/static/js/2.071232ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff786c93463a5d10922",
+    "url": "/glorified-todo/static/js/main.c6decb53.chunk.js"
+  },
+  {
+    "revision": "e61405ad4a564c75c5b4",
+    "url": "/glorified-todo/static/js/runtime-main.18086f88.js"
+  }
+]);
